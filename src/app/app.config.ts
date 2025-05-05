71f2b1e19bdfdf6f457be345a6c5a14566e7d3ea
ben-mil <benjaminhi@miltenyi.com>
2025-05-05 14:02:04 +0200

    Vorbereitung auf Angular 19 Upgrade

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,25 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInMemoryData } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+
+const heroes = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideHttpClient(),
-        importProvidersFrom([
-            HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
-                dataEncapsulation: false,
-            }),
-        ]),
-    ],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInMemoryData(() => ({ heroes }), { delay: 300 }) // Optional: delay for realism
+    )
+  ],
 };
